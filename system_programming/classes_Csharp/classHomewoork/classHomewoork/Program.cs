c89e6680d3c58cd8a9d0a64fa5f73bb954124e2c
aleksayKRV <62353328+aleksayKRV@users.noreply.github.com>
2021-01-16 16:21:57 +0700

    Add files via upload

---
+++ system_programming/classes_Csharp/classHomewoork/classHomewoork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace classHomewoork
+{
+    public class book
+    { 
+        public string author = "Гордон Д.";
+        protected string ID = "231";
+        internal int year = 1978;
+        private string colorOfCheese = "yellow";
+    }
+   
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            book book1 = new book();
+            Console.WriteLine("Автор:");
+            Console.WriteLine(book1.author);
+            Console.WriteLine("Год:");
+            Console.WriteLine(book1.year);
+            Console.ReadKey();
+        }
+    }
+}
